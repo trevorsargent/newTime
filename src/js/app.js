c44713c472d7b52bb735a616aa7a21d2bcf72005
Trevor Sargent <tbird1710@gmail.com>
2018-03-04 22:56:27 -0800

    add all structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,46 @@
 import * as Sum from './sumClock.js'
 
-const dispSum = document.getElementById('sum-clock-sum')
-const dispNum = document.getElementById('sum-clock-num')
+let updateTime
 
-Sum.setup()
-const updateTime = Sum.makeUpdateTime(dispSum, dispNum)
+// #1 -- SumClock
+const sumClock = document.getElementById('sumClock')
+if (sumClock) {
+  const dispSum = document.getElementById('sum')
+  const dispNum = document.getElementById('num')
+
+  Sum.setup()
+
+  updateTime = Sum.makeUpdateTime(dispSum, dispNum)
+}
+
+// #2 -- Progress (Bars)
+const progress = document.getElementById('progressClock')
+if (progress) {
+  const h = document.getElementById('h')
+  const m = document.getElementById('m')
+  const s = document.getElementById('s')
+}
+
+// #3 -- Vertical Binary
+
+// #4 -- Segmented
+
+// #5 -- Count Down
+
+// #6 -- Decimal Time
+
+// #7 -- Angles of Hands
+
+// #8 -- Digit Representation
+
+// #9 -- Angles Between the Hands
+
+// #10 - Color Time
+
+// #11 - Angle Sweep
+
+// #12 - Unix
+
 updateTime()
+
 setInterval(updateTime, 1000)
