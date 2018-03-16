001b4ef14d184f817bd847428b9db35fc8d96a3e
Trevor Sargent <tbird1710@gmail.com>
2018-03-15 17:43:30 -0700

    statements

--- src/js/angleReadoutClock.js
+++ src/js/angleReadoutClock.js
@@ -1,22 +1,18 @@
-import { hourHandFraction, minHandFraction, secHandFractionRough } from './analog.js'
-
+import { hourHandFraction, minHandFraction, secHandFraction } from './analog.js'
 
 export const makeUpdateTime = (h, m, s) => () => {
 
-	const now = new Date()
-	const hour = now.getHours()
-	const mins = now.getMinutes()
-	const secs = now.getSeconds()
-	const mili = now.getMilliseconds()
+  const now = new Date()
+  const hour = now.getHours()
+  const mins = now.getMinutes()
+  const secs = now.getSeconds()
+  const mili = now.getMilliseconds()
 
-	console.log("hi")
-	const hourAngle = Math.floor(hourHandFraction(hour, mins, secs) * 360).toString().padStart(3, "0") + "•"
-	const minAngle = Math.floor(minHandFraction(mins, secs) * 360).toString().padStart(3, "0") + "•"
-	const secAngle = Math.floor(secHandFractionRough(secs) * 360).toString().padStart(3, "0") + "•"
+  const hourAngle = Math.floor(hourHandFraction(hour, mins, secs) * 360).toString().padStart(3, '0') + '•'
+  const minAngle = Math.floor(minHandFraction(mins, secs) * 360).toString().padStart(3, '0') + '•'
+  const secAngle = Math.floor(secHandFraction(secs, mili) * 360).toString().padStart(3, '0') + '•'
 
-	h.innerText = hourAngle;
-	m.innerText = minAngle;
-	s.innerText = secAngle;
-
+  h.innerText = hourAngle
+  m.innerText = minAngle
+  s.innerText = secAngle
 }
-
